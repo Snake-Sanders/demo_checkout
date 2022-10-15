d8a4233c44c26e453c58ca864f92f9e6f0cc137f
Snake Sanders <elmarian22@gmail.com>
2022-10-15 17:49:58 +0200

    added discount handling

--- app/lib/app/checkout.ex
+++ app/lib/app/checkout.ex
@@ -52,10 +52,16 @@ defmodule App.Checkout do
     end
   end
 
+  @doc """
+  The discount is calculated first based on the quanity of a product.
+  Then the discount is applied when calculating product final price per unit.
+  """
   @impl true
   def handle_call(:total, _form, state) do
+    # todo: pass the discount to the calc_price as param
     total =
       state.cart
+      |> calc_discount(state.discounts)
       |> calc_price(state.prices)
 
     {:reply, total, state}
@@ -80,10 +86,58 @@ defmodule App.Checkout do
 
   # Assumes that all the items in the cart are valid items.
   def calc_price(cart, prices) do
-    Enum.reduce(cart, 0.0, fn {code, quantity}, acc ->
+    Enum.reduce(cart, 0.0, fn {code, quantity, disc_units, discount}, acc ->
       price = prices[code].price
-      quantity * price + acc
+
+      case disc_units do
+        0 ->
+          quantity * price + acc
+
+        _ ->
+          no_discount_cost = (quantity - disc_units) * price
+          discount_cost = disc_units * price * (100 - discount) * 0.01
+          no_discount_cost + discount_cost + acc
+      end
     end)
+  end
+
+  # Appends to each item in the cart how many units have discount
+  # how much should be discounted for each.
+  #
+  # Cart items are returned as:
+  # `{item_code, item_quantity, quantity_with_discount, discount}`
+  def calc_discount(cart, discounts) do
+    Enum.map(
+      cart,
+      fn {code, quantity} = item ->
+        {disc_units, discount} = get_discount(item, discounts)
+        {code, quantity, disc_units, discount}
+      end
+    )
+  end
+
+  @doc """
+  Since the units are added with the funtion `scan`, currently there is no need
+  for guarding this function with: `when is_integer(quantity)`
+  """
+  def get_discount({code, quantity} = _item, discounts) do
+    case discounts[code] do
+      "2-for-1" when quantity > 1 ->
+        # amount of units that this discount applies, group by pairs
+        units = div(quantity, 2)
+        # every second unit is free, 100% reduction
+        discount = 100
+        {units, discount}
+
+      "bulk-of-3" when quantity >= 3 ->
+        # from the 3rd unit all units get 5% reduction
+        discount = 5
+        {quantity, discount}
+
+      _ ->
+        # no enough units to apply a discount
+        {0, 0}
+    end
   end
 
   # load the product prices list
--- app/test/checkout_test.exs
+++ app/test/checkout_test.exs
@@ -5,10 +5,10 @@ defmodule CheckoutTest do
   alias App.Checkout
 
   def get_price_rules() do
-    [
-      {"2-for-1", [VOUCHER]},
-      {"bulk-of-3", [TSHIRT]}
-    ]
+    %{
+      "VOUCHER" => "2-for-1",
+      "TSHIRT" => "bulk-of-3"
+    }
   end
 
   describe "without discount" do
@@ -26,37 +26,109 @@ defmodule CheckoutTest do
 
     test "adding 3 units of one item to the cart" do
       {:ok, pid} = Checkout.new()
-      Checkout.scan(pid, "VOUCHER") # 5€
-      Checkout.scan(pid, "VOUCHER") # 5€
-      Checkout.scan(pid, "VOUCHER") # 5€
+      Checkout.scan(pid, "VOUCHER")
+      Checkout.scan(pid, "VOUCHER")
+      Checkout.scan(pid, "VOUCHER")
       assert Checkout.total(pid) == 15.0
     end
 
     test "invalid item is not added to the cart" do
       {:ok, pid} = Checkout.new()
-      Checkout.scan(pid, "TSHIRT") # 20€
-      Checkout.scan(pid, "INVALID") # 0€
-      Checkout.scan(pid, "VOUCHER") # 5€
+      Checkout.scan(pid, "TSHIRT")
+      Checkout.scan(pid, "INVALID")
+      Checkout.scan(pid, "VOUCHER")
       assert Checkout.total(pid) == 25.0
     end
   end
 
   describe "with discount" do
-    @tag :skip
+    test "select a discount for bulk-of-3" do
+      rules = get_price_rules()
+      assert Checkout.get_discount({"TSHIRT", 0}, rules) == {0, 0}
+      assert Checkout.get_discount({"TSHIRT", 1}, rules) == {0, 0}
+      assert Checkout.get_discount({"TSHIRT", 3}, rules) == {3, 5}
+      assert Checkout.get_discount({"TSHIRT", 4}, rules) == {4, 5}
+      assert Checkout.get_discount({"TSHIRT", 7}, rules) == {7, 5}
+    end
+
+    test "select a discount for 2-of-1" do
+      rules = get_price_rules()
+      assert Checkout.get_discount({"VOUCHER", 0}, rules) == {0, 0}
+      assert Checkout.get_discount({"VOUCHER", 1}, rules) == {0, 0}
+      assert Checkout.get_discount({"VOUCHER", 2}, rules) == {1, 100}
+      assert Checkout.get_discount({"VOUCHER", 3}, rules) == {1, 100}
+      assert Checkout.get_discount({"VOUCHER", 4}, rules) == {2, 100}
+      assert Checkout.get_discount({"VOUCHER", 7}, rules) == {3, 100}
+    end
+
     test "apply discount 2-for-1" do
       rules = get_price_rules()
       {:ok, pid} = Checkout.new(rules)
+
+      Checkout.scan(pid, "VOUCHER")
+      assert Checkout.total(pid) == 5
+
+      Checkout.scan(pid, "VOUCHER")
+      assert Checkout.total(pid) == 5
+
+      Checkout.scan(pid, "VOUCHER")
+      assert Checkout.total(pid) == 10
+
+      Checkout.scan(pid, "VOUCHER")
+      assert Checkout.total(pid) == 10
+    end
+
+    test "apply discount bulk-of-3" do
+      rules = get_price_rules()
+      {:ok, pid} = Checkout.new(rules)
+
       Checkout.scan(pid, "TSHIRT")
       assert Checkout.total(pid) == 20
 
       Checkout.scan(pid, "TSHIRT")
-      assert Checkout.total(pid) == 38 # 2u at 19 € each
+      assert Checkout.total(pid) == 40
 
       Checkout.scan(pid, "TSHIRT")
-      assert Checkout.total(pid) == 58 # 2u at 19 € + 1u full price
+      assert Checkout.total(pid) == 57
 
       Checkout.scan(pid, "TSHIRT")
-      assert Checkout.total(pid) == 76 # 4u at 19 € each
+      assert Checkout.total(pid) == 76
+    end
+
+    test "discount for mixed items: Example 1" do
+      rules = get_price_rules()
+      {:ok, pid} = Checkout.new(rules)
+
+      Checkout.scan(pid, "VOUCHER")
+      Checkout.scan(pid, "TSHIRT")
+      Checkout.scan(pid, "MUG")
+      assert Checkout.total(pid) == 32.50
+    end
+
+    test "discount for mixed items: Example 2" do
+      rules = get_price_rules()
+      {:ok, pid} = Checkout.new(rules)
+
+      Checkout.scan(pid, "TSHIRT")
+      Checkout.scan(pid, "TSHIRT")
+      Checkout.scan(pid, "TSHIRT")
+      Checkout.scan(pid, "VOUCHER")
+      Checkout.scan(pid, "TSHIRT")
+      assert Checkout.total(pid) == 81.0
+    end
+
+    test "discount for mixed items: Example 3" do
+      rules = get_price_rules()
+      {:ok, pid} = Checkout.new(rules)
+
+      Checkout.scan(pid, "VOUCHER")
+      Checkout.scan(pid, "TSHIRT")
+      Checkout.scan(pid, "VOUCHER")
+      Checkout.scan(pid, "VOUCHER")
+      Checkout.scan(pid, "MUG")
+      Checkout.scan(pid, "TSHIRT")
+      Checkout.scan(pid, "TSHIRT")
+      assert Checkout.total(pid) == 74.50
     end
   end
 end
